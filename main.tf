68a87c73f45e8ebe2a72c3b93b3e5d6570f43584
Manish Shahi <53034936+manishshahi64@users.noreply.github.com>
2024-06-02 10:25:47 +0530

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.51.1"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_vpc" "myvpc" {
     cidr_block = var.vpc_cidr
 }
